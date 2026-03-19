242c04bc03b1aeaef56194a84b03e584558e4ca3
soroushpiri-glitch <soroush.piri@morgan.edu>
2026-03-19 10:57:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
     page_icon="panda.jpg",
     layout="wide"
 )
-col1, col2 = st.columns([1, 8])
+col1, col2 = st.columns([1, 6])
 
 with col1:
     st.image("panda.jpg", width=70)
